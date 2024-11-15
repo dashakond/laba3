caf72682ed1f1f575c1300578785778a3660f56f
daryna <dasha.kondratez@gmail.com>
2024-11-15 13:56:41 +0200

    Initial commit with basic structure

---
+++ FuncA.cpp
@@ -0,0 +1,7 @@
+// FuncA.cpp
+#include "FuncA.h"
+
+double FuncA::calculate() {
+    return 0;  // Initial implementation returning 0
+}
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+// main.cpp
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+    FuncA func;
+    std::cout << "Result: " << func.calculate() << std::endl;
+    return 0;
+}
+
